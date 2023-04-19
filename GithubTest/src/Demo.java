d0528bdc72dc424eb95069076256105d6227d879
suba <subashinimca@gmail.com>
2023-04-18 23:28:14 -0400

    my first commit in eclipse

---
+++ GithubTest/src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+	}
+
+}
